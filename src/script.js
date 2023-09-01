495c63f5babc1d392d3b4bbc56bfe7d84f4bb163
Luis Solano <lsolano@goconsultores.com>
2023-09-01 09:57:34 -0600

    Funcionalidad para exportar un timelog CSV al formato de timelog de Excel.

--- src/script.js
+++ src/script.js
@@ -1,110 +1,117 @@
-$(document).ready(function () {
-  $("#exportWorksheet").click(function () {
-    var josnData = $("#josnData").val();
-    var jsonDataObject = eval(josnData);
-    exportWorksheet(jsonDataObject);
-  });
-
-  $("#exportWorksheetPlus").click(function () {
-    var josnData = $("#josnData").val();
-    var jsonDataObject = eval(josnData);
-    exportWSPlus(jsonDataObject);
-  });
-});
-
-function exportWorksheet(jsonObject) {
-  var myFile = "myFile.xlsx";
-  var myWorkSheet = XLSX.utils.json_to_sheet(jsonObject);
-  var myWorkBook = XLSX.utils.book_new();
+// Exporta un Arreglo de objetos (JSON) a un Excel
+const exportWorksheet = (jsonObject, myFile = "timelog.xlsx") => {
+  let myWorkSheet = XLSX.utils.json_to_sheet(jsonObject);
+  let myWorkBook = XLSX.utils.book_new();
   XLSX.utils.book_append_sheet(myWorkBook, myWorkSheet, "myWorkSheet");
-  XLSX.writeFile(myWorkBook, myFile);
-}
+  XLSX.writeFile(myWorkBook, `${myFile}.xlsx`);
+};
 
-function exportWSPlus(jsonObject) {
-  var myFile = "myFilePlus.xlsx";
-  var myWorkSheet = XLSX.utils.json_to_sheet(jsonObject);
-  XLSX.utils.sheet_add_aoa(myWorkSheet, [["Your Mesage Goes Here"]], {
-    origin: 0,
-  });
-  var merges = (myWorkSheet["!merges"] = [{ s: "A1", e: "D1" }]);
-  var myWorkBook = XLSX.utils.book_new();
-  XLSX.utils.book_append_sheet(myWorkBook, myWorkSheet, "myWorkSheet");
-  XLSX.writeFile(myWorkBook, myFile);
-}
-
+// Codifica los caracteres especiales a utf8
 const convert_to_utf8 = (code = "") => {
   return decodeURIComponent(escape(code));
 };
 
-// Return array of string values, or NULL if CSV string not well formed.
-function CSVtoArray1(text) {
-  var re_valid =
+// Convierte las líneas de un CSV en un Array
+const CSVtoArray = (text) => {
+  let re_valid =
     /^\s*(?:'[^'\\]*(?:\\[\S\s][^'\\]*)*'|"[^"\\]*(?:\\[\S\s][^"\\]*)*"|[^,'"\s\\]*(?:\s+[^,'"\s\\]+)*)\s*(?:,\s*(?:'[^'\\]*(?:\\[\S\s][^'\\]*)*'|"[^"\\]*(?:\\[\S\s][^"\\]*)*"|[^,'"\s\\]*(?:\s+[^,'"\s\\]+)*)\s*)*$/;
-  var re_value =
+  let re_value =
     /(?!\s*$)\s*(?:'([^'\\]*(?:\\[\S\s][^'\\]*)*)'|"([^"\\]*(?:\\[\S\s][^"\\]*)*)"|([^,'"\s\\]*(?:\s+[^,'"\s\\]+)*))\s*(?:,|$)/g;
-  // Return NULL if input string is not well formed CSV string.
   if (!re_valid.test(text)) return null;
-  var a = []; // Initialize array to receive values.
-  text.replace(
-    re_value, // "Walk" the string using replace with callback.
-    function (m0, m1, m2, m3) {
-      // Remove backslash from \' in single quoted values.
-      if (m1 !== undefined) a.push(m1.replace(/\\'/g, "'"));
-      // Remove backslash from \" in double quoted values.
-      else if (m2 !== undefined) a.push(m2.replace(/\\"/g, '"'));
-      else if (m3 !== undefined) a.push(m3);
-      return ""; // Return empty string.
-    }
-  );
-  // Handle special case of empty last value.
+  let a = [];
+  text.replace(re_value, (m0, m1, m2, m3) => {
+    if (m1 !== undefined) a.push(m1.replace(/\\'/g, "'"));
+    else if (m2 !== undefined) a.push(m2.replace(/\\"/g, '"'));
+    else if (m3 !== undefined) a.push(m3);
+    return "";
+  });
   if (/,\s*$/.test(text)) a.push("");
   return a;
-}
+};
 
-function parseCSV(text) {
-  // Obtenemos las lineas del texto
+// Divide el CSV en línea y luego las formatea
+const parseCSV = (text) => {
   let lines = text.replace(/\r/g, "").split("\n");
   return lines.map((line) => {
-    // Por cada linea obtenemos los valores
     line_converted = convert_to_utf8(line);
     line_converted = line_converted.replaceAll(`""`, "");
-    let values = CSVtoArray1(line_converted);
+    let values = CSVtoArray(line_converted);
     return values;
   });
-}
+};
 
-/* function reverseMatrix(matrix){
-  let output = [];
-  // Por cada fila
-  matrix.forEach((values, row) => {
-    // Vemos los valores y su posicion
-    values.forEach((value, col) => {
-      // Si la posición aún no fue creada
-      if (output[col] === undefined) output[col] = [];
-      output[col][row] = value;
-    });
-  });
-  return output;
-}
- */
-function readFile(evt) {
+// Lee el archivo
+const readFile = (evt) => {
+  const month = document.getElementById("month").value;
+  const year = document.getElementById("year").value;
+  const firstName = document.getElementById("firstName").value;
+  const lastName = document.getElementById("lastName").value;
+  const analyst = document.getElementById("analyst").value;
+  const environment = document.getElementById("environment").value;
+  const internal = document.getElementById("internal").value;
+  const formValues = {
+    month,
+    year,
+    firstName,
+    lastName,
+    analyst,
+    environment,
+    internal,
+  };
+  console.log(formValues);
   let file = evt.target.files[0];
   let reader = new FileReader();
   reader.onload = (e) => {
-    // Cuando el archivo se terminó de cargar
     let lines = parseCSV(e.target.result);
-    console.log(lines);
-    /* let output = reverseMatrix(lines);
-    console.log(output); */
+    let newLines = [];
+    let newLineHeader = [];
+    newLineHeader[0] = "Project";
+    newLineHeader[1] = "Issue Type";
+    newLineHeader[2] = "Key";
+    newLineHeader[3] = "Summary";
+    newLineHeader[4] = "Analista";
+    newLineHeader[5] = "Enviroment";
+    newLineHeader[6] = "Interno";
+    newLineHeader[7] = "Date Started";
+    newLineHeader[8] = "Display Name";
+    newLineHeader[9] = "Time Spent (h)";
+    newLineHeader[10] = "Work Description";
+    newLines.push(newLineHeader);
+    for (let i = 1; i < lines.length; i++) {
+      if (lines[i].length) {
+        let newLine = [];
+        newLine[0] = lines[i][8];
+        newLine[1] = lines[i][6];
+        newLine[2] = lines[i][3];
+        newLine[3] = lines[i][9];
+        newLine[4] = formValues.analyst;
+        newLine[5] = formValues.environment;
+        newLine[6] = formValues.internal;
+        newLine[7] = lines[i][4];
+        newLine[8] = lines[i][1];
+        newLine[9] = lines[i][0] / 60;
+        newLine[10] = lines[i][7];
+        newLines.push(newLine);
+      }
+    }
+    const timelog = [];
+    for (let i = 1; i < newLines.length; i++) {
+      let obj = {};
+      for (let j = 0; j < newLines[0].length; j++) {
+        obj = {
+          ...obj,
+          ...{ [newLines[0][j]]: newLines[i][j] },
+        };
+      }
+      timelog.push(obj);
+    }
+
+    let fileName = `${formValues.month}_${formValues.year}_${formValues.firstName}_${formValues.lastName}`;
+    console.log(fileName);
+    let jsonDataObject = eval(timelog);
+    exportWorksheet(jsonDataObject, "Agosto_2023_Luis_Solano");
   };
-  // Leemos el contenido del archivo seleccionado
   reader.readAsBinaryString(file);
-}
+};
 
 document.getElementById("file").addEventListener("change", readFile, false);
-
-console.log(convert_to_utf8("Se corrigiÃ³")); // Se corrigió
-console.log(convert_to_utf8("Se corrigiÃ³")); // Se corrigió
-// let stringTest = `30,"Luis Solano",2ee36de2-65bb-6b56-b5a2-986328861417,38294,2023-08-24,2023-W34,"Desarrollo","Se quitó la frase ""Reporte de"" del dashboard principal.","Equipos APLI","FIX: Quitar ""Reporte de"" en el dashboard principal"`
-// console.log(stringTest.replaceAll(`""`, ''))
-// console.log(exportAsExcelFile())
